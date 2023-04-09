368ff7e9b81e14553075a716aa755b4dec41eb46
EllyNg65 <elizabethngxinyi@gmail.com>
2023-04-09 20:40:05 +0800

    Complete Shop and Cart pages as in PedroTech tutorial

---
+++ src/products.js
@@ -0,0 +1,52 @@
+import product1 from './assets/black_shirt.jpg';
+import product2 from './assets/blue_shirt.jpg';
+import product3 from './assets/crew_neck_long_sleeve_shirt.jpg'
+import product4 from './assets/grey_crew_neck_tshirt.jpg'
+import product5 from './assets/purple_button_up_shirt.jpg'
+import product6 from './assets/white_long_sleeve_shirt.jpg'
+import product7 from './assets/white_shirt.jpg'
+
+export const PRODUCTS = [
+    {
+        id: 1, 
+        productName: "Black Shirt",
+        price: 50.0,
+        productImage: product1,
+    },
+    {
+        id: 2, 
+        productName: "Blue Shirt",
+        price: 55.0,
+        productImage: product2,
+    },
+    {
+        id: 3, 
+        productName: "Crew Neck Long Sleeve Shirt",
+        price: 60.0,
+        productImage: product3,
+    },
+    {
+        id: 4, 
+        productName: "Grey Crew Neck T-Shirt",
+        price: 45.0,
+        productImage: product4,
+    },
+    {
+        id: 5, 
+        productName: "Purple Button Up Shirt",
+        price: 50.0,
+        productImage: product5,
+    },
+    {
+        id: 6, 
+        productName: "White Long Sleeve Shirt",
+        price: 45.0,
+        productImage: product6,
+    },
+    {
+        id: 7, 
+        productName: "White Shirt",
+        price: 40.0,
+        productImage: product7,
+    },
+]
